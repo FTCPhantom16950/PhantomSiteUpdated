735e1b74bfb4c3053708c9544e230284bc966d2a
FTCPhantom16950 <phantom16950@gmail.com>
2026-02-06 00:44:25 +0300

    start commit

---
+++ BackendSite/src/main/java/io/github/ftcphantom16950/backendsite/BackendSiteApplication.java
@@ -0,0 +1,13 @@
+package io.github.ftcphantom16950.backendsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/// start
+@SpringBootApplication
+public class BackendSiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendSiteApplication.class, args);
+    }
+
+}
---
+++ BackendSite/src/test/java/io/github/ftcphantom16950/backendsite/BackendSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.ftcphantom16950.backendsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendSiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
