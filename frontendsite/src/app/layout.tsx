0a8e824bbaeb4ebb875b108a7c9d9749cacbc349
FTCPhantom16950 <phantom16950@gmail.com>
2026-02-12 21:14:00 +0300

    deleted original data

--- frontendsite/src/app/layout.tsx
+++ frontendsite/src/app/layout.tsx
@@ -25,7 +25,9 @@ export default function RootLayout({
 }) {
     return (
         <html lang="en">
-        <body>{children}</body>
+        <body>{children}
+XYI
+        </body>
         </html>
     )
 }
