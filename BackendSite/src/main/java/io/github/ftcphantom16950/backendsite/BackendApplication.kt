7991d9a6de3ed9660bbc1e17a81ec88dba2e4a19
FTCPhantom16950 <phantom16950@gmail.com>
2026-02-12 21:08:49 +0300

    deleted original data

---
+++ BackendSite/src/main/java/io/github/ftcphantom16950/backendsite/BackendApplication.kt
@@ -0,0 +1,11 @@
+package io.github.ftcphantom16950.backendsite
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<BackendApplication>(*args)
+}
