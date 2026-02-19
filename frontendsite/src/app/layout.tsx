8e89f202e45eea34b9508cf22cb91e01b6aea173
FTCPhantom16950 <phantom16950@gmail.com>
2026-02-19 21:55:39 +0300

    test background

--- frontendsite/src/app/layout.tsx
+++ frontendsite/src/app/layout.tsx
@@ -15,6 +15,9 @@ export default function RootLayout({
     return (
         <html lang="en">
         <body>{children}
+        <div id="back">
+
+        </div>
         </body>
         </html>
     )
